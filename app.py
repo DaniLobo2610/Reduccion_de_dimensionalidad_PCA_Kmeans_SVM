79a9af24b52058b0938f71c757a3761a8346c71e
DaniLobo2610 <137654960+DaniLobo2610@users.noreply.github.com>
2026-07-05 00:10:30 -0600

    Update app.py

--- app.py
+++ app.py
@@ -153,7 +153,7 @@ st.header("Configuración del modelo")
 n_componentes = st.slider(
     "Número de componentes principales (PCA)",
     min_value=30,
-    max_value=70,
+    max_value=35,
     value=30
 )
 
